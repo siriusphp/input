a1d011eed5861e18f9c0be124fb5973683a4eb04
Adrian Miu <adrian@adrianmiu.ro>
2014-04-09 22:45:10 +0300

    Moved files around to make things work with PHPStorm

---
+++ phpunit_bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+require_once('autoload.php');
+require_once('vendor/autoload.php');
+date_default_timezone_set('UTC');
--- tests/bootstrap.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once ('../autoload.php');
-require_once ('../vendor/autoload.php');
-date_default_timezone_set('UTC');
