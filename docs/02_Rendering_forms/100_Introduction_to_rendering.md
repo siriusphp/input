61eaa6ecde528d4baea38f211febe93069dc0dc4
Adrian Miu <adrian@adrianmiu.ro>
2014-04-24 00:12:47 +0300

    Removed the base renderer from it's namespace as you only need on renderer

--- docs/02_Rendering_forms/100_Introduction_to_rendering.md
+++ docs/02_Rendering_forms/100_Introduction_to_rendering.md
@@ -8,9 +8,9 @@ echo $formRenderer->render($form);
 
 To reach this goal the render process works like this:
 
-1. The renderer object contains a `WidgetFactory` which take a form element and generates an widget. 
+1. The renderer object contains a `WidgetFactory` which take a form element and generates an widget.
 2. The widget created by the factory is a DOM-like object that has a string representation (through `__toString()`) and contains other DOM-like objects (the label, the input, the error message etc). The form itself is a widget that contains other widgets as its children. The widgets have a [very simple interface](../10_API/Widget.md).
-3. The form renderer has a collection of `Decorators` which receive the widgets for manipulation. 
+3. The form renderer has a collection of `Decorators` which receive the widgets for manipulation.
 
 The `Decorators` and `WidgetFactories` are independent. Theoretically you can have decorators that can work with widgets produced by other widget factories.
 
@@ -36,6 +36,6 @@ The process of rendering a form is broken into the following steps
 2. The widget factory uses different "workers" to process the form and its element into widgets. A worker might be in charge of creating the label field, another responsible for constructing the input field etc.
 3. The form widget is passed to the registered decorators for... decoration. The decorators are also executed recursively the form's children.
 
-While you can achieve the same result without using decorators and using only workers, there is a distinction between "workers" and "decorators": "workers" are smart, "decorators" are dumb. 
+While you can achieve the same result without using decorators and using only workers, there is a distinction between "workers" and "decorators": "workers" are smart, "decorators" are dumb.
 
 Workers are very aware of their environment. When they process form element they are have knowledge about the form they are coming from (for example they can extract data from the validator), and about the factory itself (they can issue a request to the factory for another element if they need that). On the other hand, decorators only work with the resulting widget; they may add an HTML attribute here and there, translate some piece of data but the operations they perform are very simple.
--- src/Renderer.php
+++ src/Renderer.php
@@ -1,14 +1,13 @@
 <?php
-namespace Sirius\Forms\Renderer;
+namespace Sirius\Forms;
 
 use Sirius\Forms\Decorator\DecoratorInterface;
 use Sirius\Forms\Form;
 use Sirius\Forms\Html\ExtendedTag;
-use Sirius\Forms\Util\PriorityList;
 use Sirius\Forms\WidgetFactory\Base as BaseFactory;
 use Sirius\Forms\WidgetFactory\FactoryInterface;
 
-class Basic
+class Renderer
 {
 
     /**
@@ -29,20 +28,6 @@ class Basic
             $widgetFactory = new BaseFactory();
         }
         $this->widgetFactory = $widgetFactory;
-        $this->decoratorsList = new PriorityList();
-    }
-
-    /**
-     * Add a decorator to the stack
-     *
-     * @param DecoratorInterface $decorator
-     * @param int $priority
-     * @return self
-     */
-    function addDecorator(DecoratorInterface $decorator, $priority = 0)
-    {
-        $this->decoratorsList->add($decorator, $priority);
-        return $this;
     }
 
     /**
@@ -64,7 +49,6 @@ class Basic
     function getFormWidget(Form $form)
     {
         $widget = $this->widgetFactory->createWidget($form);
-        $this->decorateWidget($widget);
         return $widget;
     }
 
@@ -76,46 +60,13 @@ class Basic
      * @throws \RuntimeException
      * @return NULL|\Sirius\Forms\Html\ExtendedTag
      */
-    function renderElement(Form $form, $elementName)
+    function getElementWidget(Form $form, $elementName)
     {
         $element = $form->get($elementName);
         if (!$element) {
             throw new \RuntimeException(sprintf('Input "%s" is not registered to this form'));
         }
         $widget = $this->widgetFactory->createWidget($form, $element);
-        $this->decorateWidget($widget);
-        return $widget;
-    }
-
-    /**
-     * Applies the decorators to the widget
-     *
-     * @param ExtendedTag $widget
-     * @throws \LogicException
-     * @return ExtendedTag
-     */
-    protected function decorateWidget($widget)
-    {
-        if (!$widget instanceof ExtendedTag) {
-            return $widget;
-        }
-        /* @var $decorator \Sirius\Forms\Decorator\DecoratorInterface */
-        foreach ($this->decoratorsList->getIterator() as $decorator) {
-            $decoratedWidget = $decorator->decorate($widget);
-            if (!$decoratedWidget instanceof ExtendedTag) {
-                throw new \LogicException(
-                    'A decorator returned something that is not an \Sirius\Forms\Html\ExtendedTag object'
-                );
-            }
-        }
-
-        // If the widget has children, decorate them as well
-        if ($widget instanceof Sirius\Forms\Renderer\WidgetTrait\HasChildrenTrait) {
-            foreach ($widget->getChilren() as $childWidget) {
-                $this->decorate($childWidget);
-            }
-        }
-
         return $widget;
     }
 }
