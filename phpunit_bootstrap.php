516adde1321daa76dea3bce7ca496be8ae7bfb7d
Adrian Miu <adrian@adrianmiu.ro>
2015-02-04 11:56:07 +0200

    Moved some things around

--- phpunit_bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once('autoload.php');
-require_once('vendor/autoload.php');
-date_default_timezone_set('UTC');
-error_reporting(E_ALL);
---
+++ tests/phpunit_bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+require_once('../autoload.php');
+require_once('../vendor/autoload.php');
+date_default_timezone_set('UTC');
+error_reporting(E_ALL);
