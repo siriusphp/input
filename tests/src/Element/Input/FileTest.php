8bc2a9535f77309a1c3787cf673c36e5d0a5f596
Adrian Miu <adrian@adrianmiu.ro>
2015-11-30 14:58:50 +0200

    Fix errors with PHP 6&7

--- tests/src/Element/Input/FileTest.php
+++ tests/src/Element/Input/FileTest.php
@@ -21,7 +21,7 @@ class FileTest extends \PHPUnit_Framework_TestCase
 
     function setUp()
     {
-        $this->form  = m::mock('\Sirius\Input\InputFilter', array());
+        $this->form  = m::mock('\Sirius\Input\InputFilter');
         $this->input = new File('picture');
         $this->input->setUploadContainer('/var/www');
         $this->input->setUploadOptions(array(
